19c629881aaf8514c0f71ba871479b7f1ff929a7
Krish Chaudhary <161269571+Krish14505@users.noreply.github.com>
2024-07-30 12:04:00 -0400

    created the customers class with some column name as the table

---
+++ lib/customers.dart
@@ -0,0 +1,27 @@
+
+import 'package:floor/floor.dart';
+
+///This class is dedicated as the customer table.
+@entity
+class customers {
+
+  static int ID = 1 ;
+
+  ///Primary key of the customers table and other variables
+  @primaryKey
+  final int customer_id ;
+  final String first_name;
+  final String last_name;
+  final String email;
+  final String phoneNumber;
+
+
+  ///constructor
+  customers(this.customer_id,this.first_name,this.last_name,this.email,this.phoneNumber) {
+
+    if (customer_id > ID){
+      ID = customer_id + 1;
+    }
+  }
+
+}
